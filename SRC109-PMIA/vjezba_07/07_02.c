f8c519b111b3676e3753b27eed20e37279bb338a
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-05-16 12:09:46 +0200

    Fixed SRC109/07
    scanf() does not support whitespaces
    fgets() does support them and is more idiomatic

--- SRC109-PMIA/vjezba_07/07_02.c
+++ SRC109-PMIA/vjezba_07/07_02.c
@@ -10,27 +10,34 @@ njegovu duljinu.
 char *longest(int *lenght);
 
 int main(void) {
-  int lenght = 0;
+  int lenght;
   printf("Najdulji string: %s", longest(&lenght));
   printf(" [%d]\n", lenght);
   return 0;
 }
 
 char *longest(int *lenght) {
-  char *str = (char *)malloc(256);
-  char *longest_str = (char *)malloc(256);
+  const int MAX_STR_LEN = 256;
+  char *str = (char *)malloc(MAX_STR_LEN);
+  char *longest_str = (char *)malloc(MAX_STR_LEN);
 
   printf("Unesi broj stringova: ");
   int n;
   scanf(" %d", &n);
   printf("Unesi stringove:\n");
+  getchar();
 
   for (int i = 0; i < n; i++) {
     printf("\t[%d]: ", i);
-    scanf(" %255s", str);
+    fgets(str, MAX_STR_LEN - 1, stdin);
+
+    /* removes the '\n' at the end that fgets() saves */
+    str[strlen(str) - 1] = '\0';
+
     if (strlen(str) > strlen(longest_str))
       strcpy(longest_str, str);
   }
+
   *lenght = strlen(longest_str);
   return longest_str;
 }

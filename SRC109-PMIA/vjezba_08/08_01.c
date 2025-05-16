5bf2199b040780e0d1d94579a2267dca2a7af89a
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-05-16 16:14:57 +0200

    Fixed SRC109/08
    Wrongly calculated position of the character in the string

--- SRC109-PMIA/vjezba_08/08_01.c
+++ SRC109-PMIA/vjezba_08/08_01.c
@@ -10,7 +10,7 @@ funkcija vraća -1 i NULL. Koristiti funkciju strchr.
 Primjer : Za string "ONoffON" pozicija drugog pojavljivanja slova ’O’ je 6.
 */
 
-char *get_char_pos(char str[], char c, int *pos);
+char *get_second_char_pos(char str[], char c, int *pos);
 
 int main(void) {
   char string_a[] = "ONoffON";
@@ -18,8 +18,8 @@ int main(void) {
   char c2 = 'o';
   int c_pos, c2_pos;
 
-  char *c_ptr = get_char_pos(string_a, c, &c_pos);
-  char *c2_ptr = get_char_pos(string_a, c2, &c2_pos);
+  char *c_ptr = get_second_char_pos(string_a, c, &c_pos);
+  char *c2_ptr = get_second_char_pos(string_a, c2, &c2_pos);
 
   printf("string_a (%c): %s | char: %p [%d]\n", c, string_a, c_ptr, c_pos);
   printf("string_a (%c): %s | char: %p [%d]\n", c2, string_a, c2_ptr, c2_pos);
@@ -27,23 +27,22 @@ int main(void) {
   return 0;
 }
 
-char *get_char_pos(char str[], char c, int *pos) {
+char *get_second_char_pos(char str[], char c, int *pos) {
   *pos = 0;
 
-  char *f = strchr(str, c);
-  if (!f) {
+  char *first = strchr(str, c);
+  if (!first) {
     *pos = -1;
     return NULL;
   }
-  f++;
+  first++;
 
-  *pos = strlen(f);
-
-  char *s = strchr(f, c);
-  if (!s) {
+  char *second = strchr(first, c);
+  if (!second) {
     *pos = -1;
     return NULL;
   }
+  *pos = strlen(str) - strlen(second + 1);
 
-  return s;
+  return second;
 }
